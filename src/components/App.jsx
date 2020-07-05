9801847cf346685d48509a44d8f6666ed500f080
Anton Lapin <antonlapin@Antons-MacBook-Pro.local>
2020-07-05 23:27:39 +0300

    Movie component is done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,25 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Movie from "./Movie";
-import Container from '@material-ui/core/Container';
-// import Grid from '@material-ui/core/Grid';
+import Grid from '@material-ui/core/Grid';
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 <Header />
-                <Container maxWidth="sm">
-                    <Movie />
-                </Container>
+
+                <Grid
+                    container
+                    direction="row"
+                    justify="space-evenly"
+                    alignItems="center"
+                    className="content"
+                >
+                    <Movie title="Avengers" image="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg" year="2012" link="https://www.imdb.com/title/tt4154796/" />
+                </Grid>
                 <Footer />
+
             </div>
         );
     }
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,15 +1,37 @@
 import React from "react";
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
-const DEFAULT_IMAGE = "https://static.thenounproject.com/png/2999524-200.png";
 
 const Movie = (props) => {
-    // const poster = (props.poster === null ? DEFAULT_IMAGE : props.poster);
     return (
-        <div className="movie">
-            <h3 className="movie-title">Movie title</h3>
-            <img width="300" alt="" src={DEFAULT_IMAGE}></img>
-            <p>2013</p>
-        </div>
+        <Card className="movie">
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    alt="Movie"
+                    image={props.image}
+                    title={props.title}
+                    height="400px"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2" align="center">
+                        {props.title}
+                    </Typography>
+                    <Typography gutterBottom color="textSecondary" variant="h6" component="p" align="center">
+                        {props.year}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActionArea>
+                <Typography gutterBottom variant="h6" component="h3" align="center" color="primary">
+                    <a href={props.link}>Learn more</a>
+                </Typography>
+            </CardActionArea>
+        </Card>
     );
 };
 
